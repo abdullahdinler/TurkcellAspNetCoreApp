ba2d166b6b2b71ef9c408935ef8cf5b7ebe9f238
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2023-01-28 22:46:04 +0300

    Add project files.

---
+++ AspNetCoreApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreApp.Web.Models;
+
+namespace AspNetCoreApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Context c = new Context();
+
+        public IActionResult Index()
+        {
+           var result =  c.Products.ToList();
+            return View(result);
+        }
+    }
+}
---
+++ AspNetCoreApp.Web/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreApp.Web.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TurkcellAspNetCoreAppDB;Integrated Security=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
