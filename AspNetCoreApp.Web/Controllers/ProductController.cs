79998ba2268420ad1f5e99ac8e0d3fe2725b2733
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2023-01-30 01:16:37 +0300

    Temel ekleme silme ve güncelleme işlemi yapıldı

--- AspNetCoreApp.Web/Controllers/ProductController.cs
+++ AspNetCoreApp.Web/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using AspNetCoreApp.Web.Models;
 
@@ -9,12 +10,53 @@ namespace AspNetCoreApp.Web.Controllers
 {
     public class ProductController : Controller
     {
-        private Context c = new Context();
+        private readonly Context _c = new Context();
 
+       
         public IActionResult Index()
         {
-           var result =  c.Products.ToList();
+            var result = _c.Products.ToList();
             return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+
+            _c.Products.Add(product);
+            _c.SaveChanges();
+            TempData["Alert"] = "Ürün başarılı bir şekilde eklendi";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _c.Products.Find(id);
+            _c.Products.Remove(product);
+            TempData["Alert"] = "Ürün başarılı bir şekilde silindi.";
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var product = _c.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            _c.Products.Update(product);
+            _c.SaveChanges();
+            TempData["Alert"] = "Ürün başarılı bir şekilde güncellendi.";
+            return RedirectToAction("Index");
         }
     }
 }
